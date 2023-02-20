ec1c8a23f2552982047e465f1419d1f9472a6f38
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-02-20 04:41:49 -0800

    bugFix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,10 @@ int main(void)
 
 	for (n = 0; n < 9; n++)
 	{
-		for (i = 1; i < 10; i++)
+		for (i = n + 1; i < 10; i++)
 		{
+			if (n == i)
+				continue;
 			putchar('0' + n);
 			putchar('0' + i);
 			if (n == 8 && i == 9)
