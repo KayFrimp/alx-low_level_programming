80a9f6a7964158a671893ecce8ccedae0f968dd9
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-02-23 03:06:52 -0800

    Adding integers is easy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: integer
+ * @y: integer
+ *
+ * Return: Always the result of sum of x and y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
