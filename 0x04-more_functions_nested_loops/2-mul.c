542c9c9891d3ff5996e8a90dfc720e0f0c7bb262
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-02-24 03:34:20 -0800

    product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: Integer value 1
+ * @b: Integer value 2
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,5 @@ MORE FUNCTIONS, MORE NESTED LOOPS
 0. 0-isupper.c checks for uppercase character.
 
 1. 1-isdigit.c checks for a digit (0 through 9).
+
+2. 2-mul.c multiplies two integers.
