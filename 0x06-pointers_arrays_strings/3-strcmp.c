41be2f4d20289854d7940594843697daba9d70b8
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-02 20:13:09 -0800

    strcmp mimicked

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: Character pointer variable
+ * @s2: Character pointer variable
+ *
+ * Return: 0, +/-number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, res;
+
+	if (s1 == s2)
+		return (0);
+	for (i = 0, j = 0; *(s1 + i) != '\0' || *(s2 + j) != '\0'; i++, j++)
+	{
+		if (*(s1 + i) == *(s2 + j))
+			continue;
+		if (*(s1 + i) < *(s2 + j))
+		{
+			res = (*(s1 + i) - *(s2 + j));
+			break;
+		}
+		if (*(s1 + i) > *(s2 + j))
+		{
+			res = (*(s1 + i) - *(s2 + j));
+			break;
+		}
+	}
+	
+	return (res);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -5,3 +5,5 @@ POINTERS, ARRAYS & STRINGS
 1. 1-strncat.c - Concatenates two strings using at most n bytes specified for the second string.
 
 2. 2-strncpy.c - copies the string pointed to by src, including the terminating null byte to the buffer pointed to by dest.
+
+3. 3-strcmp.c - Compares two strings like the standard library function strcmp.
