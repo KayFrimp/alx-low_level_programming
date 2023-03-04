0569be5aabbc8529eafdd94a1fbc38f6ef086e7a
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-04 07:39:47 -0800

    bugFix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,12 +18,10 @@ char *cap_string(char *str)
 		{
 			if (*(str + i) == delim[j])
 			{
-				if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
+				if (*(str + i + 1) >= 97  && *(str + i + 1) <= 122)
 				{
 					*(str + i + 1) = *(str + i + 1) - 32;
 				}
-				else
-					continue;
 			}
 		}
 	}
