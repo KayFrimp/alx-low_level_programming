ccad06358a06cc9d3c9ed5c21216327ac029963f
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-08 16:51:06 -0800

    String length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (counter() + _strlen_recursion(s + 1));
+	return (0);
+}
+
+/**
+ * counter - counter
+ *
+ * Return: Always 1
+ */
+int counter(void)
+{
+	return (1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,5 @@
 0. 0-puts_recursion.c is a function that prints a string, followed by a new line.
 
 1. 1-print_rev_recursion.c is a function that prints a string in reverse.
+
+2. 2-strlen_recursion.c - function returns the length of a string.
