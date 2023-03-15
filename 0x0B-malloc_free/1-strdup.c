ea2406409a752b0327c3620cecfff9b6f9aa9d05
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-14 22:21:54 -0700

    BugFix_02

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		return (NULL);
 	while (str[len] != '\0')
 		len++;
-	dstr = malloc(len * sizeof(char));
+	dstr = malloc((len + 1) * sizeof(char));
 	if (dstr == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
