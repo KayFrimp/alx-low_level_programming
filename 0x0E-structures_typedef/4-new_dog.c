cab341dd5091ed894d6eb72d41580691ac1cb46c
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-21 06:18:10 -0700

    Memory alloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,20 +13,43 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL);
-	if (name == NULL || owner == NULL)
+                return (NULL);
+	
+	dog->name = malloc((_strlen(name) * sizeof(char)) + 1);
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	else
+	dog->owner = malloc((_strlen(owner) * sizeof(char)) + 1);
+	if (dog->owner == NULL)
 	{
-		dog->name = name;
-		dog->age = age;
-		dog->owner = owner;
+		free(dog->name);
+		free(dog);
+		return (NULL);
 	}
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
 
 	return (dog);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: Char pointer variable
+ *
+ * Return: Always the length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
 }
