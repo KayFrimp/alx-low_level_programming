e0c1fa5e8888646f97b8fa8e4525b4b5f648c0a9
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-20 19:50:05 -0700

    Poppy

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,3 @@
 0x0E. C-Structures, typedf
+
+0. dog.h - Defines a new type struct dog with name, age and owner.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Description: This is a struct of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
