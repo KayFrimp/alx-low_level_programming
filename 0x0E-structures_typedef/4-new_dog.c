4347e3b853749830b15a794440464cae6972d808
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-21 06:21:12 -0700

    betty in check

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-                return (NULL);
-	
+		return (NULL);
+
 	dog->name = malloc((_strlen(name) * sizeof(char)) + 1);
 	if (dog->name == NULL)
 	{
