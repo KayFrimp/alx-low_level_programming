dae8ab2ceb05ad29fa97a38cbd276a3ccca18248
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-23 20:08:14 -0700

    print strings success

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,15 +12,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list string_list;
 	unsigned int i;
+	char *str;
 
 	va_start(string_list, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(string_list, char *));
+		str = va_arg(string_list, char *);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
-		if (separator == NULL)
-			printf("(nil");
 	}
 	va_end(string_list);
 	printf("\n");
