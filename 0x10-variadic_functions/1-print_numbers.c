c1104eaffca777d262f1a8afddd8a2f556a5bdcb
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-03-23 19:43:57 -0700

    separator check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_numbers - Prints numbers, followed by a new line
@@ -14,16 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(numbers, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(numbers, unsigned int));
-			if (i != n - 1)
-				printf("%s", separator);
-		}
-		va_end(numbers);
+		printf("%d", va_arg(numbers, unsigned int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	va_end(numbers);
 	printf("\n");
 }
