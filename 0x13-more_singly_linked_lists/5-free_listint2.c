5e14b1e2e8005af786f588ed0664c1fcd96c4851
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-04-04 14:56:24 -0700

    Alternate

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,12 +13,10 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-	temp = *head;
-	while (temp != NULL)
+	while (*head != NULL)
 	{
-		*head = temp->next;
-		free(temp);
-		temp = *head;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
