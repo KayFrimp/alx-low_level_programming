2089bbaf9e321b86e16b5c99376283f96d016d2d
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-04-17 14:43:07 -0700

    num to bin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints binary of a number
+ * @n: Number to print in binary
+ *
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int bit;
+
+	if (n > 1)
+		print_binary(n >> 1);
+
+	bit = n & 1;
+	_putchar(n & 1 + '0');
+}
