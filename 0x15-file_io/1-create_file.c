be7f0eb5acf8b8ec739510e1961b8b51a6306732
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-05-09 04:09:58 -0700

    bugFix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	w_bytes = write(fd, text_content, len);
-	if (w_bytes == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		w_bytes = write(fd, text_content, len);
+		if (w_bytes == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
