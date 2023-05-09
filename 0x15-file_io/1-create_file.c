2b921bc7316d2313e35edd774f6fda30c34cdf32
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-05-09 04:05:18 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: Null terminated string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len = 0;
+	ssize_t w_bytes;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len] != '\0')
+		len++;
+	/* create file and maintain the permissions if it already exists */
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	w_bytes = write(fd, text_content, len);
+	if( w_bytes == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,5 @@
 0x15 C - FILE I/O
 
 0. 0-read_textfile.c - Function reads a text file and prints it to the POSIX standard output.
+
+1. 1-create_file.c - Function creates a file.
