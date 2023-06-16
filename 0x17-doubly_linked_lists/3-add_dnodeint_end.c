a9cacd9fd5ae42372775c37e7007e4504114878f
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-15 17:23:24 -0700

    Add node at end of dlistint

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
+ * @head: pointer to head pointer of dlistint_t list
+ * @n: Integer value of new node
+ *
+ * Return: Address of the new element or NUll
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *ptr;
+
+	if (*head == NULL)
+		return (add_firstdnodeint(head, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	ptr = *head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+
+	ptr->next = new_node;
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = ptr;
+
+	return (new_node);
+}
+
+/**
+ * add_firstdnodeint - Adds first node to a dlistint_t list
+ * @head: pointer to head pointer of dlistint_t list
+ * @n: Integer value of first node
+ *
+ * Return: Address of first node or NULL
+ */
+dlistint_t *add_firstdnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *first_node = malloc(sizeof(dlistint_t));
+
+	if (first_node == NULL)
+		return (NULL);
+
+	first_node->prev = NULL;
+	first_node->n = n;
+	first_node->next = NULL;
+	*head = first_node;
+	return (*head);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -7,3 +7,5 @@
 1. 1-dlistint_len.c - Returns number of elements in a dlistint_t list.
 
 2. 2-add_dnodeint.c - Adds a new node at the beginning of a dlistint_t list.
+
+3. 3-add_dnodeint_end.c - Adds a new node at the end of a dlistint_t list.
